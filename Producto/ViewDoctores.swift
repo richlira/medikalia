08535e92fbbb06b3c717433f95fe9a74eb7959b1
R Mac User <macuser@Rs-MacBook-Pro.local>
2017-03-23 19:43:36 -0600

    Primer Commit

---
+++ Producto/ViewDoctores.swift
@@ -0,0 +1,35 @@
+//
+//  ViewDoctores.swift
+//  Producto
+//
+//  Created by R Mac User on 3/23/17.
+//  Copyright © 2017 medicalia. All rights reserved.
+//
+
+import UIKit
+
+class ViewDoctores: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
